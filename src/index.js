8c7a08b15e4028d100a70d7db2c975bb3a532f2d
Shaik_Z <kz981786@gmail.com>
2020-12-05 18:17:23 +0530

    c13

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-const onePageArticleCount = 10
+// const onePageArticleCount = 10
 const { newsArticleModel } = require('./connector');
 
 // Parse JSON bodies (as sent by API clients)
@@ -12,14 +12,17 @@ app.use(express.json());
 let defaultLimit= 10, defaultOffset= 0;
 let start=0;
 app.get('/newFeeds',(req,res)=>{
-    let limit= parseInt(req.query.limit);
-    let offset= parseInt(req.query.offset);
+    let limit= (req.query.limit);
+    let offset= (req.query.offset);
+    // console.log(typeof limit);
     if(!limit){
         limit= defaultLimit;
     }
     if(!offset){
         offset= defaultOffset;
     }
+    limit=parseInt(limit);
+    offset= parseInt(offset);
     newsArticleModel.find().then(result=>{
         start+= offset;
         res.json(result.splice(start,start+limit));
